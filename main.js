1fc5a4b6dc4a3bf0daf7e6fc5abb5953df5a965a
Kyler <kyler.b@live.com>
2021-11-16 09:26:21 -0600

    commit tree ed8c520b0d03ada1c6a4818b59c58913b906f958

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+const likeButtons = document.querySelectorAll(".like-glyph");
+function init(){
+  likeButtons.forEach(button => function(){
+    button.addEventListener("click", function(){
+      mimicServerCall()
+        .then(()=>{
+          if(button.textContent == EMPTY_HEART){
+            button.classList.add("activated-heart");
+            console.log(button.classList);
+            button.textContent = FULL_HEART;
+          }else{
+            button.classList.remove("activated-heart");
+            console.log(button.classList);
+            button.textContent = EMPTY_HEART;
+          }
+        })
+        .catch(()=>{
+          document.querySelector("#modal").className = "";
+          setTimeout(function(){
+            return document.querySelector("#modal").className = "hidden";
+          }, 3000);
+        });
+    });
+  }());
+}
+init();
 
 
 
